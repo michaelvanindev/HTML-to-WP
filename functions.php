e812dd5c0c944ff93262b98a0bb9e569ce35fd83
Michael Vanin <67514946+michaelvanindev@users.noreply.github.com>
2024-06-25 13:18:09 -0230

    load additional head tags

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@
 
 // Includes
 include(get_theme_file_path('/includes/front/enqueue.php'));
+include(get_theme_file_path('/includes/front/head.php'));
 
 // Hooks
 add_action('wp_enqueue_scripts', 'mv_enqueue');
+add_action('wp_head', 'mv_head', 5);
